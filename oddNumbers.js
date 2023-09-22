b1fbac1c22e7c6316c02b2e3f456a273c20998a7
EliasFernandes03 <elias.neto@dunasystem.com>
2023-09-22 14:24:14 -0300

    odd counterback

--- oddNumbers.js
+++ oddNumbers.js
@@ -1,6 +1,14 @@
-let minhaLista = []
+// let minhaLista = []
 
-for (let index = 1; index < 10; index+=2) {
-    minhaLista.push(index)
+// for (let index = 1; index < 10; index+=2) {
+//     minhaLista.push(index)
+// }
+// console.log(minhaLista)
+
+
+const myArray = [];
+
+for (let i = 9; i >= 1; i -= 2) {
+    myArray.push(i);
 }
-console.log(minhaLista)
+console.log(myArray)
