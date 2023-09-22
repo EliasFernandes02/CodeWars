a51cbf8c6d2300e63bfa9616f6dd8940c1b24aea
EliasFernandes03 <elias.neto@dunasystem.com>
2023-09-22 13:45:05 -0300

    mangoes

---
+++ mangoes.js
@@ -0,0 +1,13 @@
+function mango(quantity, price){
+    if(quantity ===2){
+      total = quantity * price
+      return total
+       } else if(quantity === 3){
+        total = (quantity*price)-3
+    }else if(quantity === 5){
+        total = (quantity*price)-3
+    }else if(quantity === 9){
+        total = (quantity*price)-9
+    }
+    
+  }
